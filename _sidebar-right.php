f837d631f7e5f583171957d23de999e7bb7dd005
mario9881 <mario_9881@abv.bg>
2021-09-27 16:41:08 +0300

    Added "Support" and "Get Help" in plugin's settings menu in right sidebar Asana task: https://app.asana.com/0/37680704590859/1201045010987204/f

---
+++ _sidebar-right.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Right sidebar for settings page
+ *
+ * @package   DX_Template_Manager
+ * @link      http://devrix.com
+ * @copyright 2021 DevriX
+ */
+?>
+
+<?php
+/**
+ *-----------------------------------------
+ * Do not delete this line
+ * Added for security reasons: http://codex.wordpress.org/Theme_Development#Template_Files
+ *-----------------------------------------
+ */
+defined('ABSPATH') or die("Direct access to the script does not allowed");
+?>
+
+
+	<div id="postbox-container-1" class="postbox-container sidebar-right">
+	<div class="meta-box-sortables">
+		<div class="postbox">
+			<h3><span><?php esc_attr_e('Get help','dx-template-options');?></span></h3>
+			<div class="inside">
+				<div>
+					<ul>
+						<li><a target="_blank" href="https://wordpress.org/plugins/dx-template-manager/"><span class="dashicons dashicons-admin-home"></span> <?php esc_attr_e( 'Plugin Homepage', 'dx-template-options' );?></a></li>
+                        <li><a target="_blank" href="https://devrix.com/contact/"><span class="dashicons dashicons-email-alt"></span> <?php esc_attr_e( 'Contact us', 'dx-template-options' );?></a></li>
+                        <li><a target="_blank" href="https://wordpress.org/support/plugin/dx-template-manager/"><span class="dashicons dashicons-groups"></span> <?php esc_attr_e( 'Support', 'dx-template-optionsn' );?></a></li>
+                        <li><a target="_blank" href="https://wordpress.org/support/plugin/dx-template-manager/reviews/"><span class="dashicons dashicons-star-filled"></span> <?php esc_attr_e( 'Review', 'dx-template-optionsn' );?></a></li>
+                    </ul>
+				</div>
+				<div class="sidebar-footer">
+					&copy; <?php echo date('Y');?> <a class="no-underline text-highlighted" href="http://devrix.com" title="DevriX" target="_blank">DevriX</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+
+<?php
--- dx-template-admin-view.php
+++ dx-template-admin-view.php
@@ -1,6 +1,6 @@
 <div id="icon-plugins" class="icon32"></div>
-
-<div class='wrap'>
+<div class='dx-template-wrapper'>
+<div class='wrap content-wrapper'>
 	<h2>DX Template</h2>
 
 	<p><strong>I realize that the plugin is using eval() and theoretically this could lead to malware script insertion
@@ -14,32 +14,9 @@
 			
 			<input type="submit" class="button button-primary" value="Save" />
 
-        <div class="wrap">
-            <h1><strong>DX Template Support</strong></h1>
-
-            <p>Do you like DX Template Manager, you need support or additional features/functionality? Help us out by leaving us a review!</p>
-            <a class="button-primary" target="_blank" href="https://wordpress.org/support/plugin/dx-template-manager/">Give a review</a>
-
-        </div>
-	</form> <!-- end of #dxtemplate-form -->
-</div>
-<p></p>
-<div id="postbox-container-1" class="postbox-container">
-    <div class="meta-box-sortables">
-        <div class="postbox">
-            <h3><span>&emsp;Get help</span></h3>
-            <div class="inside">
-                <div>
-                    <ul>
-                        <li><a target="_blank" href="https://wordpress.org/plugins/dx-template-manager/"><span class="dashicons dashicons-admin-home"></span></a><b> Plugin Homepage</b></li>
-                        <li><a target="_blank" href="https://wordpress.org/plugins/dx-template-manager/#installation"><span class="dashicons dashicons-media-text"></span></a><b> Documentation</b></li>
-                        <li><a target="_blank" href="https://devrix.com/contact/"><span class="dashicons dashicons-sos"></span></a><b> Contact us</b></li>
-                    </ul>
-                </div>
-                <div class="sidebar-footer">
-                    <b>© 2021</b> <a class="no-underline text-highlighted" href="http://devrix.com" title="DevriX" target="_blank"><b>DevriX</b></a>
-                </div>
-            </div>
-        </div>
     </div>
+	</form> <!-- end of #dxtemplate-form -->
+
+	<?php include_once( '_sidebar-right.php' );?>
+
 </div>
